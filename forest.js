907742bc18e83a0cbc48b50fcc66e790d399ca4e
Daria Diachkova <daria.diachkova@mail.ru>
2022-03-18 11:38:54 +0300

    feat(forest): add new tracks

--- forest.js
+++ forest.js
@@ -2,6 +2,11 @@ const backgroundColour = '#1A202C'
 let rainbow
 let pooratNaLes
 let dragons
+let crowander
+let monster
+let giant
+let male
+let donkey
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -9,6 +14,11 @@ function setup() {
   rainbow = loadSound('assets/rainbow.mp3');
   pooratNaLes = loadSound('assets/poorat-na-les.mp3');
   dragons = loadSound('assets/dragons.mp3');
+  crowander = loadSound('assets/crowander.mp3');
+  monster = loadSound('assets/monster.mp3');
+  giant = loadSound('assets/giant.mp3');
+  male = loadSound('assets/male.mp3');
+  donkey = loadSound('assets/donkey.mp3');
   noStroke();
   textSize(15);
   background(backgroundColour);
@@ -53,6 +63,7 @@ function keyPressed() {
     background(backgroundColour);
   } else if ([49,50,51,52,53,54,55,56,57].includes(pressed)) {
     switch (pressed) {
+      // 1
       case 49:
         if (rainbow.isPlaying()) {
           rainbow.stop();
@@ -60,6 +71,7 @@ function keyPressed() {
           rainbow.play();
         }
         break;
+      // 2
       case 50:
         if (pooratNaLes.isPlaying()) {
           pooratNaLes.stop();
@@ -67,11 +79,52 @@ function keyPressed() {
           pooratNaLes.play();
         }
         break;
+      // 3
       case 51:
         if (dragons.isPlaying()) {
           dragons.stop();
         } else {
           dragons.play();
+        }
+        break;
+      // 4
+      case 52:
+        if (crowander.isPlaying()) {
+          crowander.stop();
+        } else {
+          crowander.play();
+        }
+        break;
+      // 5
+      case 53:
+        if (monster.isPlaying()) {
+          monster.stop();
+        } else {
+          monster.play();
+        }
+        break;
+      // 6
+      case 54:
+        if (giant.isPlaying()) {
+          giant.stop();
+        } else {
+          giant.play();
+        }
+        break;
+      // 7
+      case 55:
+        if (male.isPlaying()) {
+          male.stop();
+        } else {
+          male.play();
+        }
+        break;
+      // 8
+      case 56:
+        if (donkey.isPlaying()) {
+          donkey.stop();
+        } else {
+          donkey.play();
         }
         break;
       default: null
