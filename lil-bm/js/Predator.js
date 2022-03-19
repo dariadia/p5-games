56cae1c821e109572017c7b2674eeb6df14a3d1e
Daria Diachkova <daria.diachkova@mail.ru>
2022-03-19 17:11:39 +0300

    feat(Predator): add sample moves

--- lil-bm/js/Predator.js
+++ lil-bm/js/Predator.js
@@ -29,6 +29,57 @@ class Predator {
     this.campfirePosY = y;
   }
 
+  move() {
+    this.vx = map(noise(this.tx), 0, 1, -this.speed, this.speed);
+    this.vy = map(noise(this.ty), 0, 1, -this.speed, this.speed);
+
+    if (this.vx < 0) {
+      this.faceLeft = true;
+    } else if (this.vx > 0) {
+      this.faceLeft = false;
+    }
+
+    this.health -= this.healthLossPerMove;
+    this.health = constrain(this.health, 0, this.maxHealth);
+
+    this.x += this.vx;
+    this.y += this.vy;
+
+    this.tx += 0.01;
+    this.ty += 0.01;
+
+    this.handleWrapping();
+
+    if (this.health <= 0) {
+      this.reset();
+    }
+  }
+
+  collide(object) {
+    let d = dist(this.x, this.y, object.x, object.y);
+    let dx = object.x - this.x;
+    let dy = object.y - this.y;
+    let angle = atan2(dy, dx);
+    if (d < this.radius + object.radius / 2) {
+      this.x -= this.speed / 3.5 * Math.cos(angle);
+      this.y -= this.speed / 3.5 * Math.sin(angle);
+    }
+  }
+
+  handleWrapping() {
+    if (this.x < 0) {
+      this.x += width;
+    } else if (this.x > width) {
+      this.x -= width;
+    }
+
+    if (this.y < 0) {
+      this.y += height;
+    } else if (this.y > height) {
+      this.y -= height;
+    }
+  }
+
 
 
   display(inGame) {
