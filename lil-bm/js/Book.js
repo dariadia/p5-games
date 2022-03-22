63203fb8c5f39c04cdf2b60db335b0f043eef054
Daria Diachkova <daria.diachkova@mail.ru>
2022-03-22 19:41:07 +0300

    feat(Books): add collision

--- lil-bm/js/Book.js
+++ lil-bm/js/Book.js
@@ -5,6 +5,17 @@ class Book extends Tree {
     this.health = this.maxHealth;
   }
 
+  collide(object) {
+    let d = dist(this.x, this.y, object.x, object.y);
+    let dx = object.x - this.x;
+    let dy = object.y - this.y;
+    let angle = atan2(dy, dx);
+    if (d < this.radius + object.radius / 2) {
+      this.x -= random(1, 2) / 3.5 * Math.cos(angle);
+      this.y -= random(1, 2) / 3.5 * Math.sin(angle);
+    }
+  }
+
   reset() {
     this.x = random(0, width);
     this.y = random(0, height);
