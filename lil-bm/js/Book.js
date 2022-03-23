5877cfdfb66f255f76672880ac7ef7602fef817d
Daria Diachkova <daria.diachkova@mail.ru>
2022-03-23 19:19:03 +0300

    fix(Book)

--- lil-bm/js/Book.js
+++ lil-bm/js/Book.js
@@ -1,12 +1,13 @@
-class Book extends Tree {
+// Book
+// can be collected by Character (player)
+
+class Book {
   constructor(x, y, radius, texture) {
     this.x = x;
     this.y = y;
+    this.health = radius; 
     this.texture = texture;
     this.radius = radius;
-
-    this.maxHealth = 100;
-    this.health = this.maxHealth;
   }
 
   collide(object) {
@@ -16,13 +17,37 @@ class Book extends Tree {
     let angle = atan2(dy, dx);
     if (d < this.radius + object.radius / 2) {
       this.x -= random(1, 2) / 3.5 * Math.cos(angle);
-      this.y -= random(1, 2) / 3.5 * Math.sin(angle);
+      this.y -= random(1, 2)/ 3.5 * Math.sin(angle);
     }
+  }
+
+  handleWrapping() {
+    if (this.x < 0) {
+      this.x += width;
+    } else if (this.x > width) {
+      this.x -= width;
+    }
+
+    if (this.y < 0) {
+      this.y += height;
+    } else if (this.y > height) {
+      this.y -= height;
+    }
+  }
+
+  display() {
+    push();
+    rectMode(CORNER);
+    noStroke();
+    imageMode(CENTER);
+
+    image(this.texture, this.x, this.y, this.radius, this.radius);
+    pop();
   }
 
   reset() {
     this.x = random(0, width);
     this.y = random(0, height);
-    this.health = this.maxHealth;
+    this.health = this.radius;
   }
 }
