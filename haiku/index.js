40330bb2672eed0971ee54bf6684fdd13d7b0ef9
Daria Diachkova <daria.diachkova@mail.ru>
2022-04-03 20:59:48 +0300

    feat(haiku): style

--- haiku/index.js
+++ haiku/index.js
@@ -8,7 +8,7 @@ function setup() {
   rg.loadFrom('/haiku/assets/grammar-en.json')
 
   button = createButton('generate')
-  button.class('grid')
+  button.class('block my-8 mx-auto bg-teal-500 hover:bg-teal-400 text-white font-bold py-2 px-4 border-b-4 border-teal-700 hover:border-teal-500 rounded')
   button.mousePressed(newHaiku)
 }
 
